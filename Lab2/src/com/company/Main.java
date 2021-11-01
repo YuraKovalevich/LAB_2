a133662bfd880683491f979d3f5d2f259b7fdbac
Yurochka <kovalevichyura@gmail.com>
2021-11-01 19:15:37 +0300

    added window

--- Lab2/src/com/company/Main.java
+++ Lab2/src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        JFrame frame= new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
